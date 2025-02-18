dfce0c32c0add66ad287e1400c4875c9afab3065
shota <s8152.45@gmail.com>
2025-02-18 11:20:09 +0900

    Add: ループのカルーセル（スライダ）

--- vite-project/src/main.js
+++ vite-project/src/main.js
@@ -1,24 +1,12 @@
 import "./style.css";
-import javascriptLogo from "./javascript.svg";
-import viteLogo from "/vite.svg";
-import { setupCounter } from "./counter.js";
 
-document.querySelector("#app").innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!!!!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`;
+// Splide
+import Splide from "@splidejs/splide";
+import "@splidejs/splide/css";
+import "@splidejs/splide/css/skyblue";
+import "@splidejs/splide/css/sea-green";
+import "@splidejs/splide/css/core";
 
-setupCounter(document.querySelector("#counter"));
+new Splide(".splide", {
+	type: "loop",
+}).mount();
