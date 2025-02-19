f28add26e58dc1d6a55e1d149526e6dbd2a847ff
shota <s8152.45@gmail.com>
2025-02-19 09:59:19 +0900

    Add: interfaceの拡張（extends）

--- typescript-test/src/test.ts
+++ typescript-test/src/test.ts
@@ -1,46 +1,79 @@
 export const test = () => {
-	console.log("test");
+	// console.log("test");
 
-	const name: string = "Mike"; //string
-	console.log(name);
-	const age: number = 25; //number
-	const isStudent: boolean = false; //boolean
-	const hobbies: string[] = ["Reading", "Coding", "Gaming"];
-	const address: object = {
-		street: "123 Main St",
-		city: "Los Angeles",
-		state: "Ca",
-	}; //object
-	const today: Date = new Date(); //date
-	const none: null = null; //null
-	const noDefined: undefined = undefined; //undefined
-	const random: any = "Hello World"; //any
-	const gender: "male" | "female" = "male"; //union
-	const value: string | number | null = "Hello World"; //union
+	// const name: string = "Mike"; //string
+	// console.log(name);
+	// const age: number = 25; //number
+	// const isStudent: boolean = false; //boolean
+	// const hobbies: string[] = ["Reading", "Coding", "Gaming"];
+	// const address: object = {
+	// 	street: "123 Main St",
+	// 	city: "Los Angeles",
+	// 	state: "Ca",
+	// }; //object
+	// const today: Date = new Date(); //date
+	// const none: null = null; //null
+	// const noDefined: undefined = undefined; //undefined
+	// const random: any = "Hello World"; //any
+	// const gender: "male" | "female" = "male"; //union
+	// const value: string | number | null = "Hello World"; //union
 
-	//問題：引数に文字列の配列を受け取り、その中身の順番を逆にして返す関数を作成する
-	// 例：reverseArray["a", "b", "c"] => ["c", "b", "a"]
-	// 実際にデータ型を定義してみよう：変数、引数、返り値
-	// オプション引数
-	const reverseArray = (arr: string[], hasTom?: boolean): string[] => {
-		const reversedArray: string[] = [];
+	// //問題：引数に文字列の配列を受け取り、その中身の順番を逆にして返す関数を作成する
+	// // 例：reverseArray["a", "b", "c"] => ["c", "b", "a"]
+	// // 実際にデータ型を定義してみよう：変数、引数、返り値
+	// // オプション引数
+	// const reverseArray = (arr: string[], hasTom?: boolean): string[] => {
+	// 	const reversedArray: string[] = [];
 
-		let index: number = arr.length - 1;
-		while (index >= 0) {
-			reversedArray.push(arr[index]);
-			index--;
-		}
+	// 	let index: number = arr.length - 1;
+	// 	while (index >= 0) {
+	// 		reversedArray.push(arr[index]);
+	// 		index--;
+	// 	}
 
-		if (hasTom) {
-			reversedArray.push("Tom");
-		}
-		return reversedArray;
+	// 	if (hasTom) {
+	// 		reversedArray.push("Tom");
+	// 	}
+	// 	return reversedArray;
+	// };
+
+	// console.log(reverseArray(["a", "b", "c"], true));
+
+	// const reverseArray2 = (arr: string[]): string[] => {
+	// 	return arr.reverse();
+	// };
+	// console.log(reverseArray2(["a", "b", "c", "d"]));
+
+	const user = {
+		userId: 1,
+		name: "Mike",
+		age: 25,
+		email: "maike@gmail.com",
+		isActive: true,
 	};
 
-	console.log(reverseArray(["a", "b", "c"], true));
+	interface UserProps {
+		userId: number;
+		name: string;
+		age: number;
+		email: string;
+		isActive: boolean;
+	}
 
-	const reverseArray2 = (arr: string[]): string[] => {
-		return arr.reverse();
+	interface AdminUser extends UserProps {
+		role: string; //"admin", "leader", "manager"
+	}
+
+	const createAccount = (accountInfo: AdminUser) => {
+		//
 	};
-	console.log(reverseArray2(["a", "b", "c", "d"]));
+
+	createAccount({
+		userId: 1,
+		name: "Mike",
+		age: 25,
+		email: "maike@gmail.com",
+		isActive: true,
+		role: "admin",
+	});
 };
